72c28570e743f556eb7d0ba9def8a60e8abb592e
Alessandro Angelino <alessandro.angelino@arm.com>
2015-01-21 10:49:15 +0000

    Corrected code layout

--- mbed-hal-k64f/device/MK64F12/MK64F12_sim_uvisor.h
+++ mbed-hal-k64f/device/MK64F12/MK64F12_sim_uvisor.h
@@ -20,9 +20,15 @@
 #include "fsl_bitaccess.h"
 
 /* bitband address from normale address and bit position */
-#define BITBAND_ADDRESS32(Reg,Bit) ((uint32_t volatile*)(0x42000000u + (32u*((uint32_t)(Reg) - (uint32_t)0x40000000u)) + (4u*((uint32_t)(Bit)))))
-#define BITBAND_ADDRESS16(Reg,Bit) ((uint16_t volatile*)(0x42000000u + (32u*((uint32_t)(Reg) - (uint32_t)0x40000000u)) + (4u*((uint32_t)(Bit)))))
-#define BITBAND_ADDRESS8(Reg,Bit)  ((uint8_t  volatile*)(0x42000000u + (32u*((uint32_t)(Reg) - (uint32_t)0x40000000u)) + (4u*((uint32_t)(Bit)))))
+#define BITBAND_ADDRESS32(Reg,Bit) ((uint32_t volatile*)(0x42000000u +\
+            (32u*((uint32_t)(Reg) - (uint32_t)0x40000000u))          +\
+            (4u*((uint32_t)(Bit)))))
+#define BITBAND_ADDRESS16(Reg,Bit) ((uint16_t volatile*)(0x42000000u +\
+            (32u*((uint32_t)(Reg) - (uint32_t)0x40000000u))          +\
+            (4u*((uint32_t)(Bit)))))
+#define BITBAND_ADDRESS8(Reg,Bit)  ((uint8_t  volatile*)(0x42000000u +\
+            (32u*((uint32_t)(Reg) - (uint32_t)0x40000000u))          +\
+            (4u*((uint32_t)(Bit)))))
 
 /* uvisor mode */
 extern const uint32_t __uvisor_mode;
@@ -38,7 +44,6 @@ static inline void uvisor_bitband(uint32_t *addr, uint32_t val)
         register uint32_t __r0 __asm__("r0") = (uint32_t) addr;
         register uint32_t __r1 __asm__("r1") = (uint32_t) val;
         __asm__ volatile(
-            "mov  r12, #2\n"
             "svc  #0\n"
             :: "r" (__r0), "r" (__r1)
 	);
@@ -46,33 +51,55 @@ static inline void uvisor_bitband(uint32_t *addr, uint32_t val)
 }
 
 /* SIM_SCGC1 */
+
 #undef  BW_SIM_SCGC1_UART4
-#define BW_SIM_SCGC1_UART4(x, v) uvisor_bitband(BITBAND_ADDRESS32(HW_SIM_SCGC1_ADDR(x), BP_SIM_SCGC1_UART4), v)
+#define BW_SIM_SCGC1_UART4(x, v) \
+  uvisor_bitband(BITBAND_ADDRESS32(HW_SIM_SCGC1_ADDR(x), BP_SIM_SCGC1_UART4), v)
+
 #undef  BW_SIM_SCGC1_UART5
-#define BW_SIM_SCGC1_UART5(x, v) uvisor_bitband(BITBAND_ADDRESS32(HW_SIM_SCGC1_ADDR(x), BP_SIM_SCGC1_UART5), v)
+#define BW_SIM_SCGC1_UART5(x, v) \
+  uvisor_bitband(BITBAND_ADDRESS32(HW_SIM_SCGC1_ADDR(x), BP_SIM_SCGC1_UART5), v)
 
 /* SIM_SCGC4 */
+
 #undef  BW_SIM_SCGC4_UART0
-#define BW_SIM_SCGC4_UART0(x, v) uvisor_bitband(BITBAND_ADDRESS32(HW_SIM_SCGC4_ADDR(x), BP_SIM_SCGC4_UART0), v)
+#define BW_SIM_SCGC4_UART0(x, v) \
+  uvisor_bitband(BITBAND_ADDRESS32(HW_SIM_SCGC4_ADDR(x), BP_SIM_SCGC4_UART0), v)
+
 #undef  BW_SIM_SCGC4_UART1
-#define BW_SIM_SCGC4_UART1(x, v) uvisor_bitband(BITBAND_ADDRESS32(HW_SIM_SCGC4_ADDR(x), BP_SIM_SCGC4_UART1), v)
+#define BW_SIM_SCGC4_UART1(x, v) \
+  uvisor_bitband(BITBAND_ADDRESS32(HW_SIM_SCGC4_ADDR(x), BP_SIM_SCGC4_UART1), v)
+
 #undef  BW_SIM_SCGC4_UART2
-#define BW_SIM_SCGC4_UART2(x, v) uvisor_bitband(BITBAND_ADDRESS32(HW_SIM_SCGC4_ADDR(x), BP_SIM_SCGC4_UART2), v)
+#define BW_SIM_SCGC4_UART2(x, v) \
+  uvisor_bitband(BITBAND_ADDRESS32(HW_SIM_SCGC4_ADDR(x), BP_SIM_SCGC4_UART2), v)
+
 #undef  BW_SIM_SCGC4_UART3
-#define BW_SIM_SCGC4_UART3(x, v) uvisor_bitband(BITBAND_ADDRESS32(HW_SIM_SCGC4_ADDR(x), BP_SIM_SCGC4_UART3), v)
+#define BW_SIM_SCGC4_UART3(x, v) \
+  uvisor_bitband(BITBAND_ADDRESS32(HW_SIM_SCGC4_ADDR(x), BP_SIM_SCGC4_UART3), v)
 
 /* SIM_SCGC5 */
+
 #undef  BW_SIM_SCGC5_PORTA
-#define BW_SIM_SCGC5_PORTA(x, v) uvisor_bitband(BITBAND_ADDRESS32(HW_SIM_SCGC5_ADDR(x), BP_SIM_SCGC5_PORTA), v)
+#define BW_SIM_SCGC5_PORTA(x, v) \
+  uvisor_bitband(BITBAND_ADDRESS32(HW_SIM_SCGC5_ADDR(x), BP_SIM_SCGC5_PORTA), v)
+
 #undef  BW_SIM_SCGC5_PORTB
-#define BW_SIM_SCGC5_PORTB(x, v) uvisor_bitband(BITBAND_ADDRESS32(HW_SIM_SCGC5_ADDR(x), BP_SIM_SCGC5_PORTB), v)
+#define BW_SIM_SCGC5_PORTB(x, v) \
+  uvisor_bitband(BITBAND_ADDRESS32(HW_SIM_SCGC5_ADDR(x), BP_SIM_SCGC5_PORTB), v)
+
 #undef  BW_SIM_SCGC5_PORTC
-#define BW_SIM_SCGC5_PORTC(x, v) uvisor_bitband(BITBAND_ADDRESS32(HW_SIM_SCGC5_ADDR(x), BP_SIM_SCGC5_PORTC), v)
+#define BW_SIM_SCGC5_PORTC(x, v) \
+  uvisor_bitband(BITBAND_ADDRESS32(HW_SIM_SCGC5_ADDR(x), BP_SIM_SCGC5_PORTC), v)
+
 #undef  BW_SIM_SCGC5_PORTD
-#define BW_SIM_SCGC5_PORTD(x, v) uvisor_bitband(BITBAND_ADDRESS32(HW_SIM_SCGC5_ADDR(x), BP_SIM_SCGC5_PORTD), v)
+#define BW_SIM_SCGC5_PORTD(x, v) \
+  uvisor_bitband(BITBAND_ADDRESS32(HW_SIM_SCGC5_ADDR(x), BP_SIM_SCGC5_PORTD), v)
+
 #undef  BW_SIM_SCGC5_PORTE
-#define BW_SIM_SCGC5_PORTE(x, v) uvisor_bitband(BITBAND_ADDRESS32(HW_SIM_SCGC5_ADDR(x), BP_SIM_SCGC5_PORTE), v)
+#define BW_SIM_SCGC5_PORTE(x, v) \
+  uvisor_bitband(BITBAND_ADDRESS32(HW_SIM_SCGC5_ADDR(x), BP_SIM_SCGC5_PORTE), v)
 
 #endif/*defined(K64F12_SERIES) && defined(__GNUC__)*/
 
-#endif/*__MK64F12_SIM_UVISOR_H__*/
+#endif/*__HW_SIM_REGISTERS_UVISOR_H__*/
